4b2dbf4302018c2b3bd79249b8417ff456a532b5
Nishchhal Prajapati <101253218+nishchhalprajapat@users.noreply.github.com>
2022-03-18 15:43:41 +0530

    Update FactorialTest.java

--- ProgramExercise/src/com/sebone/program/basic/test/FactorialTest.java
+++ ProgramExercise/src/com/sebone/program/basic/test/FactorialTest.java
@@ -1,11 +1,7 @@
 package com.sebone.program.basic.test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 import com.sebone.program.basic.Factorial;
-
 class FactorialTest {
 
 	@Test
